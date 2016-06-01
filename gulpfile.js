f63576d2259f198666c9a9022c2fccbdc9d19892
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-06-01 12:06:57 +0800

    增加threejs的实例

--- gulpfile.js
+++ gulpfile.js
@@ -10,21 +10,24 @@ gulp.task('webserver',function () {
   });
 });
 
-// gulp.task('jsLint', function () {
-//     gulp.src('./js/*.js') // path to your files
-//     .pipe(jshint())
-//     .pipe(jshint.reporter()); // Dump results
-// });
+gulp.task('jsLint', function () {
+    gulp.src('./js/*.js') // path to your files
+    .pipe(jshint())
+    .pipe(jshint.reporter()); // Dump results
+});
 
 
 gulp.task('livereload',function () {
-  watch(['d3js/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
-  watch(['d3js/**/*']).pipe(connect.reload());
-  watch(['Data_Visualazation_with_D3js/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
-  watch(['Data_Visualazation_with_D3js/**/*']).pipe(connect.reload());
+  // watch(['d3js/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
+  // watch(['d3js/**/*']).pipe(connect.reload());
+  // watch(['Data_Visualazation_with_D3js/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
+  // watch(['Data_Visualazation_with_D3js/**/*']).pipe(connect.reload());
+  //
+  // watch(['three.js/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
+  // watch(['three.js/**/*']).pipe(connect.reload());
 
-  watch(['three.js/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
-  watch(['three.js/**/*']).pipe(connect.reload());
+  watch(['threejs-essentials/js/*.js']).pipe(jshint()).pipe(jshint.reporter());
+  watch(['threejs-essentials/**/*']).pipe(connect.reload());
 });
 
 gulp.task('default',['webserver','livereload']);
